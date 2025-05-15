0131eb5f736982cf8d40b65bd0902d783c5b9851
Sila Rim <51617383+rimsila@users.noreply.github.com>
2025-05-16 00:22:29 +0700

    update repo url

--- install.js
+++ install.js
@@ -2,65 +2,63 @@ module.exports = {
   run: [
     // Edit this step to customize the git repository to use
     {
-     when: "{{gpu !== 'nvidia'}}",
-     method: "notify",
-     params: {
-       html: "This app requires an NVIDIA GPU."
-     }, 
-      next: null
+      when: "{{gpu !== 'nvidia'}}",
+      method: "notify",
+      params: {
+        html: "This app requires an NVIDIA GPU.",
+      },
+      next: null,
     },
     {
       method: "shell.run",
       params: {
         message: [
-          "git clone https://github.com/deepbeepmeep/Wan2GP app",
-        ]
-      }
+          "git clone https://github.com/awesome-service/Wan2GP.git app",
+        ],
+      },
     },
     {
       method: "shell.run",
       params: {
-        venv: "env",                // Edit this to customize the venv folder path
-        path: "app",                // Edit this to customize the path to start the shell from
-        message: [
-          "uv pip install -r requirements.txt",
-        ]
-      }
+        venv: "env", // Edit this to customize the venv folder path
+        path: "app", // Edit this to customize the path to start the shell from
+        message: ["uv pip install -r requirements.txt"],
+      },
     },
     {
       method: "script.start",
       params: {
         uri: "torch.js",
         params: {
-          venv: "env",                // Edit this to customize the venv folder path
-          path: "app",                // Edit this to customize the path to start the shell from
+          venv: "env", // Edit this to customize the venv folder path
+          path: "app", // Edit this to customize the path to start the shell from
           // xformers: true   // uncomment this line if your project requires xformers
-        }
-      }
+        },
+      },
     },
-//    {
-//      method: "shell.run",
-//      params: {
-//        venv: "env",                // Edit this to customize the venv folder path
-//        path: "app",                // Edit this to customize the path to start the shell from
-//        message: [
-//          "uv pip install numpy==1.24.4"
-//        ]
-//      }
-//    },
+    //    {
+    //      method: "shell.run",
+    //      params: {
+    //        venv: "env",                // Edit this to customize the venv folder path
+    //        path: "app",                // Edit this to customize the path to start the shell from
+    //        message: [
+    //          "uv pip install numpy==1.24.4"
+    //        ]
+    //      }
+    //    },
     {
-      method: 'input',
+      method: "input",
       params: {
-        title: 'Installation completed',
-        description: 'Click "Start" on the left sidebar to get started'
-      }
+        title: "Installation completed",
+        description: 'Click "Start" on the left sidebar to get started',
+      },
     },
 
-//    {
-//      method: "fs.link",
-//      params: {
-//        venv: "app/env"
-//      }
-//    },
-  ]
-}
+    //    {
+    //      method: "fs.link",
+    //      params: {
+    //        venv: "app/env"
+    //      }
+    //    },
+  ],
+};
